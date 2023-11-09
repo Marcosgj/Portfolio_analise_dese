1e46054e5d7826d43f6a982d486d7b4e07c11387
Marcos R G <marcosgj_70@hotmail.com>
2023-11-09 10:28:37 -0300

    Adicionar arquivos de projeto.

---
+++ Portfolio_analise_dese/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Portfolio_analise_dese
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.textBoxSum = new System.Windows.Forms.TextBox();
+            this.textBoxDivisiveisPor4 = new System.Windows.Forms.TextBox();
+            this.textBoxQuadrados = new System.Windows.Forms.TextBox();
+            this.textBoxSomaValoresPares = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(38, 62);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(1376, 22);
+            this.textBoxResult.TabIndex = 0;
+            // 
+            // textBoxSum
+            // 
+            this.textBoxSum.Location = new System.Drawing.Point(38, 104);
+            this.textBoxSum.Name = "textBoxSum";
+            this.textBoxSum.Size = new System.Drawing.Size(1376, 22);
+            this.textBoxSum.TabIndex = 1;
+            // 
+            // textBoxDivisiveisPor4
+            // 
+            this.textBoxDivisiveisPor4.Location = new System.Drawing.Point(38, 156);
+            this.textBoxDivisiveisPor4.Name = "textBoxDivisiveisPor4";
+            this.textBoxDivisiveisPor4.Size = new System.Drawing.Size(1376, 22);
+            this.textBoxDivisiveisPor4.TabIndex = 2;
+            // 
+            // textBoxQuadrados
+            // 
+            this.textBoxQuadrados.Location = new System.Drawing.Point(38, 209);
+            this.textBoxQuadrados.Multiline = true;
+            this.textBoxQuadrados.Name = "textBoxQuadrados";
+            this.textBoxQuadrados.Size = new System.Drawing.Size(1376, 145);
+            this.textBoxQuadrados.TabIndex = 3;
+            // 
+            // textBoxSomaValoresPares
+            // 
+            this.textBoxSomaValoresPares.Location = new System.Drawing.Point(38, 404);
+            this.textBoxSomaValoresPares.Name = "textBoxSomaValoresPares";
+            this.textBoxSomaValoresPares.Size = new System.Drawing.Size(1376, 22);
+            this.textBoxSomaValoresPares.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1476, 697);
+            this.Controls.Add(this.textBoxSomaValoresPares);
+            this.Controls.Add(this.textBoxQuadrados);
+            this.Controls.Add(this.textBoxDivisiveisPor4);
+            this.Controls.Add(this.textBoxSum);
+            this.Controls.Add(this.textBoxResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.TextBox textBoxSum;
+        private System.Windows.Forms.TextBox textBoxDivisiveisPor4;
+        private System.Windows.Forms.TextBox textBoxQuadrados;
+        private System.Windows.Forms.TextBox textBoxSomaValoresPares;
+    }
+}
+
---
+++ Portfolio_analise_dese/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Portfolio_analise_dese
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load; // Adicione um manipulador para o evento Load do formulário
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Limpar conteúdo textbox
+            textBoxResult.Clear();
+
+            // Frase para acompanhar o resultado
+            textBoxResult.AppendText("a) Os números ímpares de 0 a 40 são: " + Environment.NewLine);
+
+            // Apresentar os valores numéricos ímpares de 0 a 40 com espaço entre eles
+            for (int i = 1; i <= 40; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    textBoxResult.AppendText(i + ", "); // Número seguido de espaço
+                }
+            }
+
+            // Calcular a soma dos 100 primeiros números inteiros
+            int soma = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                soma += i;
+            }
+
+            // Exibir a soma nesse textbox
+            textBoxSum.Clear();
+            textBoxSum.AppendText("b) A soma dos 100 primeiros números inteiros é: " + soma);
+
+            // Apresentar os números divisíveis por 4 menores que 40
+            textBoxDivisiveisPor4.Clear();
+            textBoxDivisiveisPor4.AppendText("c) Números divisíveis por 4 que são menores que 40 são: " + Environment.NewLine);
+            for (int i = 1; i < 40; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    textBoxDivisiveisPor4.AppendText(i + ", ");
+                }
+            }
+
+            // Apresentar os quadrados dos números inteiros de 15 a 200
+            textBoxQuadrados.Clear();
+            textBoxQuadrados.AppendText("d) Quadrados dos números inteiros de 15 a 200 são: " + Environment.NewLine);
+            for (int i = 15; i <= 200; i++)
+            {
+                int quadrado = i * i;
+                textBoxQuadrados.AppendText(quadrado + ", ");
+            }
+
+            // Calcular a soma de todos os valores pares de 1 a 50
+            int somaPares = 0;
+            for (int i = 1; i <= 50; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    somaPares += i;
+                }
+            }
+
+            // Apresentar a soma dos valores nesse textbox
+            textBoxSomaValoresPares.Clear();
+            textBoxSomaValoresPares.AppendText("e) Soma de valores pares de 1 a 50 é: " + somaPares);
+
+        }
+    }
+}
